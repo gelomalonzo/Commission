dc48be7c0f8ac8aa230038d8c68c13f51ecbad54
gelomalonzo <abmalonzo@up.edu.ph>
2023-08-22 13:55:00 +0800

    Updated Schemas page and references

--- pages/Schemas.py
+++ pages/Schemas.py
@@ -13,3 +13,41 @@ with open(st.secrets["paths"]["INDEX_CSS"]) as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 with open(st.secrets["paths"]["MODULES_CSS"]) as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# ===== GLOBAL VARIABLES ===== #
+
+# ===== PAGE CONTENT ===== #
+st.header("Schemas")
+st.write("Listed below are the schema tables for retail and enterprise. Scroll through the bottom of the page for instructions.")
+st.write("---")
+ret_col, mid_col, ent_col = st.columns((1, 0.1, 1))
+
+with ret_col:
+    st.subheader("Retail")
+    st.write("For Salespersons")
+    ret_schema_df = pd.read_csv(st.secrets["paths"]["RSP_SCHEMA_DB"])
+    ret_schema_df = st.data_editor(ret_schema_df, num_rows="dynamic", use_container_width=True)
+    st.write("For Team Leaders")
+    ret_schema_df = pd.read_csv(st.secrets["paths"]["RTL_SCHEMA_DB"])
+    ret_schema_df = st.data_editor(ret_schema_df, num_rows="dynamic", use_container_width=True)
+
+with ent_col:
+    st.subheader("Enterprise")
+
+st.write("---")
+st.subheader("Instructions")
+
+with st.expander("Editing a row"):
+    st.write("To edit a row's details, you can double click on a cell, then type in the updated information for that particular row.")
+    st.write('Do not forget to click on the "Save changes" button found on the upper right of the table to save your changes on the data.')
+
+with st.expander("Adding a new row"):
+    st.write("To add a new row, scroll through the bottom-most of the table. Click on the empty last row, then type in the new data.")
+    st.write("Make sure to fill in all its fields.")
+    st.write('Do not forget to click on the "Save changes" button found on the upper right of the table to save your changes on the data.')
+
+with st.expander("Deleting a row"):
+    st.write("To delete a row, tick the checkbox corresponding to that row, then press on the keyboard's delete button.")
+    st.write("You can select multiple rows and press on the keyboard's delete button to delete multiple rows at once.")
+    st.write("You can also select all rows by clicking on the checkboxes' column header and pressing on the keyboard's delete button.")
+    st.write('Do not forget to click on the "Save changes" button found on the upper right of the table to save your changes on the data.')
