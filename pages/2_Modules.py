f93c768c5594fcb0c47c212c44d15329e3d4cf6a
gelomalonzo <abmalonzo@up.edu.ph>
2023-08-30 19:12:31 +0800

    Removed unnecessary functions

--- pages/2_Modules.py
+++ pages/2_Modules.py
@@ -24,92 +24,15 @@ modules_df = pd.read_csv(PATHS.MODULES_DB)
 modules_df = TOOLS.setDataTypes(modules_df, VARS.MODULES_DTYPES)
 
 # ===== FUNCTIONS ===== #
-def loadModulesDF(container, modules_df_editor):
-    modules_df = pd.read_csv(PATHS.MODULES_DB)
-    modules_df = TOOLS.setDataTypes(modules_df, VARS.MODULES_DTYPES)
-    
-    with container:
-        container.empty()
-        modules_df_editor = modules_df.copy()
-    return
-
-def isValidModulesCSV(modules_files):
-    # check if there is at least one file in the file upload field
-    if len(modules_files) == 0:
-        context = {
-            "status":False,
-            "messages":[
-                {"content":"Please add at least one file to upload.", "type":"warning"}
-            ]
-        }
-        return context
-    
-    context = {
-        "status":True,
-        "messages":[]
-    }
-    
-    # verify that each file contains the columns Module Name and Module Fee
-    valid_dfs = []
-    for file in modules_files:
-        filename = file.name
-        content = file.read()
-        is_valid = True
-        try:
-            content_str = str(content, "utf-8", errors="ignore")
-            df = pd.read_csv(StringIO(content_str))
-            columns = ""
-            for column in df.columns:
-                if len(columns) == 0: columns = '"' + column + '"'
-                else: columns = columns + ', "' + column + '"'
-            if "Module Name" not in df.columns:
-                message = {
-                    "content":('Column "Module Name" not found in file "' + filename + '". Found the following columns: ' + columns),
-                    "type":"error"
-                }
-                context["messages"].append(message)
-                is_valid = False
-            if "Module Fee" not in df.columns:
-                context["messages"].append({
-                    "content":('Column "Module Fee" not found in file "' + filename + '". Found the following columns: {' + columns + "}."),
-                    "type":"error"
-                })
-                is_valid = False
-        except pd.errors.EmptyDataError:
-            # context["status"] = False
-            # context["messages"].append({
-            #     "content":('The file "' + filename + '" is empty.'),
-            #     "type":"warning"
-            # })
-            is_valid = False
-        except pd.errors.ParserError:
-            context["messages"].append({
-                "content":('The file "' + filename + '" has an invalid CSV format.'),
-                "type":"error"
-            })
-            is_valid = False
-        if is_valid: valid_dfs.append({"filename":filename, "df":df})
-    
-    # default: the uploaded file is valid
-    if len(valid_dfs) == 0:
-        context["status"] = False
-    return valid_dfs, context
-
-def importModulesFromCSV(new_modules_dfs, modules_df):
-    context = {"status":True, "messages":[]}
-    
-    for new_module in new_modules_dfs:
-        new_module_filename = new_module["filename"]
-        new_module_df = new_module["df"]
-        new_module_df = TOOLS.setDataTypes(new_module_df, VARS.MODULES_DTYPES)
-        modules_df = pd.concat([modules_df, new_module_df], ignore_index=True, copy=False, verify_integrity=True)
-        modules_df.to_csv(PATHS.MODULES_DB, index=False)
-        context["messages"].append({
-            "content":('Successfully imported modules from "' + new_module_filename + '". Please refresh the page to reflect changes.'),
-            "type":"success"
-        })
-
-    return context
+def removeDuplicates(df:pd.DataFrame):
+    unique_modules = df["Module Name"].unique()
+    dup_indices = []
+    for module_name in unique_modules:
+        temp_df = df[df["Module Name"] == module_name]
+        for i, row_i in temp_df.iterrows():
+            for j, row_j in temp_df.iterrows():
+                if j > i: dup_indices.append(i)
+    return df.drop(dup_indices).sort_values(by=["Module Name"], ignore_index=True).reset_index(drop=True)
 
 # ===== PAGE CONTENT ===== #
 st.header("Modules List")
@@ -119,14 +42,16 @@ st.write("---")
 df_col, notes_col = st.columns([1, 0.5])
 with df_col:
     ctrl_row = st.container()
-    modules_df_editor = st.data_editor(modules_df, num_rows="dynamic", use_container_width=True)
+    modules_df_editor = st.data_editor(modules_df, num_rows="dynamic", use_container_width=True, hide_index=False)
     with ctrl_row:
         ctrl_btn_row = st.container()
         ctrl_msg_row = st.container()
         with ctrl_btn_row:
             if st.button("Save changes"):
-                modules_df = modules_df_editor
+                modules_df = removeDuplicates(modules_df_editor)
+                # modules_df = modules_df_editor.sort_values(by=["Module Name"]).reset_index(drop=True)
                 modules_df.to_csv(PATHS.MODULES_DB, index=False)
+                modules_df_editor = modules_df
                 TOOLS.displayAlerts(ctrl_msg_row, [{"content":"Changes saved successfully.", "type":"success"}])
             
 with notes_col:
