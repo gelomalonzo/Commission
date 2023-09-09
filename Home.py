d7df85b5f49ec1f92137d683379bdc096811d06f
gelomalonzo <abmalonzo@up.edu.ph>
2023-09-09 15:53:56 +0800

    Added downloadable MSR with withdrawn status

--- Home.py
+++ Home.py
@@ -217,10 +217,17 @@ if st.session_state.show_results:
     # SET UP COMMANDS
     with commands_col:
         st.download_button(
-            "Download MSR as CSV", 
+            "Download Passed MSR as CSV", 
             use_container_width=True, 
             data=msr_df.to_csv(index=False), 
-            file_name="msr.csv",
+            file_name="msr -passed.csv",
+            mime="csv"
+        )
+        st.download_button(
+            "Download Passed MSR as CSV", 
+            use_container_width=True, 
+            data=pd.concat([msr_df, wd_nonsoc_msr_df]).to_csv(index=False), 
+            file_name="msr -passed,wd.csv",
             mime="csv"
         )
         st.download_button(
